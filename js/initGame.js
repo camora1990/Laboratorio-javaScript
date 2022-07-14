30e61bbf6a271fae04af1f54cc90d5311c25a600
camora1990 <moorales.camilo@gmail.com>
2022-07-14 16:46:02 -0500

    [add] se crea la funcion initGame para armar todo el juego con los componntes

---
+++ js/initGame.js
@@ -0,0 +1,23 @@
+import { game } from "./components/game.js";
+import { loginForm } from "./components/loginForm.js";
+import { NavBar } from "./components/navBar.js";
+import { startModaGame } from "./components/startModaGame.js";
+
+export function initGame(userGame) {
+  const navar = new NavBar(userGame);
+
+  navar.createNavBar();
+  loginForm.createLoginForm();
+  startModaGame.createModal();
+  
+  if (!userGame.logged) {
+    loginForm.formloginNode.classList.add("d-flex");
+  } else {
+    if (userGame.category.level > 1) {
+      navar.navBar.style.display = "";
+      game.createQuestionModal(userGame.category.level)
+    } else {
+      startModaGame.starterGameModal.style.display = "";
+    }
+  }
+}
